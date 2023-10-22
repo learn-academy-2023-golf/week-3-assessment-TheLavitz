eee04ce07b5e4f489f4bc47211d9ac792cffd036
TheLavitz <lavitz@gmail.com>
2023-10-22 14:31:16 -0700

    Assessment complete

--- behavioral-questions.md
+++ behavioral-questions.md
@@ -4,16 +4,25 @@ Answer the following questions. Use the Nugget + STAR method for each answer.
 
 1. Tell me about a project or accomplishment you’re proud of.
 
-- Nugget:
-- Situation:
-- Task:
-- Action:
-- Result:
+- Nugget: Using PowerShell I was able to automate processes and shorten the timeline on a yearly project by a month.
+
+- Situation: I was working as a Desktop Support Technician and was assigned to a team of 5 technicians that had to handle the onboarding a few hundred interns.
+
+- Task: We had to find out what the interns needed, where they needed it, when they needed it, and then deliver/install/configure it.
+
+- Action: We broke the problem down into separate tasks that could be handled simultaneously by multiple technicians and I was able to write PowerShell scripts that automated the majority of Windows configuration, driver updates, software installation, and software configuration so that every intern's computer was identically configured so that troubleshooting could be kept to a minimum during installation.
+
+- Result: With a few weeks of time to prepare we were able to successfully onboard all interns as their departments brought them in and finish the project a month ahead of schedule. Because of the efficiency with which we operated we were able to find time to work on tasks unrelated to the current project in between deliveries to reduce the workload that would normally be shifted onto the rest of the department during this project each year.
+
 
 2. Tell me about a time when you had to deal with a high-pressure situation in your job.
 
-- Nugget:
-- Situation:
-- Task:
-- Action:
-- Result:
+- Nugget: As a Sonar Technician in the United States Navy I was able to use my attention to detail to determine the source of a communication system failure and bring the system back online.
+
+- Situation: Underway on a deployment our communication circuit for reporting incoming torpedoes went down. Without that communication circuit we must return to port and bring the circuit back online before we can continue the deployment.
+
+- Task: I was tasked with assisting the personnel in charge of internal communications circuits on board the ship with accessing the secure spaces that my department owned since the circuit terminated inside.
+
+- Action: After a few days of unsuccessful troubleshooting I was able to use wiring diagrams to trace the power for that communication circuit from the source where it was generated through the load centers and bus transfers to where it terminated in our space and determine where power could most likely have been terminated and locate the power panel that needed to be switched back on.
+
+- Result: Through a cool-head and an attention to detail I was able to bring the communication circuit back online after many others had reserved themselves to the fact that we would have to pause our deployment for repairs and the ship's deployment was able to continue as scheduled.
--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,15 +14,40 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibonacciLength1 = 6
+const fibonacciLength1 = 6;
 // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibonacciLength2 = 10
+const fibonacciLength2 = 10;
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+describe("fibonacciGen", () => {
+  it("Takes in a number (greater than 2) and returns an array containing the Fibonacci sequence.", () => {
+    expect(fibonacciGen(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8]);
+    expect(fibonacciGen(fibonacciLength2)).toEqual([
+      1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+    ]);
+  });
+});
+
+const fibonacciGen = (input) => {
+  if (input > 2) {
+    let fibonacciOut = [1, 1];
+    for (i = 1; i < input - 1; i++) {
+      fibonacciOut.push(fibonacciOut[i - 1] + fibonacciOut[i]);
+    }
+    return fibonacciOut;
+  }
+};
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// Declare a function called fibonacciGen
+// Input: a number greater than 2
+// Output: an array the length of the input containing the fibonacci sequence
+// Use an if() conditional to verify the input is greater than two
+// Use a for() loop to continue adding values to the output array until the array is the length of the input
+// Use the .push() built-in-method to take the sum of the current and previous index of the array and place it at the end of the array.
 
 // --------------------2) For the following prompt, use ChatGPT or an equivalent AI tool to help you solve the problem. The prompt will require the use of a JavaScript method called Object.values()
 // Prompt: Create a function that takes in an object and returns an array of the object's values sorted from least to greatest.
@@ -36,8 +61,8 @@ const studyMinutesWeek1 = {
   wednesday: 15,
   thursday: 30,
   friday: 15,
-  saturday: 60
-}
+  saturday: 60,
+};
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
 
 const studyMinutesWeek2 = {
@@ -47,10 +72,32 @@ const studyMinutesWeek2 = {
   wednesday: 60,
   thursday: 20,
   friday: 15,
-  saturday: 65
-}
+  saturday: 65,
+};
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
+
+describe("sortObjectValues", () => {
+  it("Takes an object and returns an array of the object's values sorted from least to greatest", () => {
+    expect(sortObjectValues(studyMinutesWeek1)).toEqual([
+      15, 15, 20, 30, 30, 60, 90,
+    ]);
+    expect(sortObjectValues(studyMinutesWeek2)).toEqual([
+      10, 15, 20, 45, 60, 65, 100,
+    ]);
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+const sortObjectValues = (object) => {
+  const valuesArray = Object.values(object);
+  valuesArray.sort((a, b) => a - b);
+  return valuesArray;
+};
+
 // Pseudo code:
+// Declare a function named sortObjectValues
+// Input: Object
+// Output: Array
+// Use the method Object.values to return an array of the values stored in an object
+// Use the .sort() method to sort the values of the returned array using a comparison function to ensure the numbers are sorted from smalles to largest
